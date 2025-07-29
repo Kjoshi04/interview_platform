86592a487c08ca0e54aab5a603a701d5f14ebc17
Kjoshi04 <2004kritikajoshi@gmail.com>
2025-07-29 18:41:07 +0530

    refractor: create form field as reusable components

---
+++ components/ui/FormField.tsx
@@ -0,0 +1,17 @@
+<FormField
+    control={form.control}
+    name="username"
+    render={({ field }) => (
+        <FormItem>
+            <FormLabel>Username</FormLabel>
+            <FormControl>
+                <Input placeholder="shadcn" {...field} />
+            </FormControl>
+            <FormDescription>
+                This is your public display name.
+            </FormDescription>
+            <FormMessage />
+        </FormItem>
+    )}
+/>
+<Button type="submit">Submit</Button>
