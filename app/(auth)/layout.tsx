3905a8d639eb5ed1f6089ec6665b9149ad8b1a66
Kritika Joshi <2004kritikajoshi@gmail.com>
2025-09-12 11:42:00 +0530

    Update layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,9 +4,10 @@ import {redirect} from "next/navigation";
 
 const AuthLayout = async ({children}:{children: ReactNode}) => {
     const isUserAuthenticated = await isAuthenticated();
-    if(!isUserAuthenticated) redirect('/');
+    if(isUserAuthenticated) redirect('/');
     return (
         <div className={"auth-layout"}>{children}</div>
     )
 }
 export default AuthLayout
+
